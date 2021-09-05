0231cdcfc34380a7835840243f1dd6375f7dde08
Kirusha3000 <ksidak7@gmail.com>
2021-09-05 14:45:19 +0300

    Third commit

--- data/config.py
+++ data/config.py
@@ -6,9 +6,3 @@ env.read_env()
 API_TOKEN = env.str("API_TOKEN")
 PATH = '/Users/kyryl/Desktop/users.db'
 
-
-class States(Enum):
-    S_START = 0  # Start
-    S_ENTER_NAME = 1  # User enters his name
-    S_TYPE_MESSAGE = 2  # User types a message
-    S_UPDATE_NAME = 3  # User updates his name
